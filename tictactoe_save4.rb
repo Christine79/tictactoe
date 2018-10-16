4deed590337c0029e3f90219ac07bc7331cf2bf7
christine <christinejaouen@hotmail.com>
2018-10-16 22:30:34 +0200

    3ème commit

--- tictactoe_save4.rb
+++ tictactoe_save4.rb
@@ -2,13 +2,6 @@ class Game
 
     attr_accessor :player1, :player2, :playerTP,  :turn_typed
 
-    def initialize(player1, player2, playerTP)
-      @player1 = player1
-      @player2 = player2
-      @palyerTP = playerTP
-    end
-
-
 #verification de la saisie du tour joué
 
    def turn_verification(turn_typed)
@@ -21,25 +14,22 @@ class Game
 
         l = turn_typed[0]
         n = turn_typed[1]
-        alpha_found = /[A-C]/.match(l)
-        num_found = /[0-3]/.match(n)
+        alpha_found = /[A-D]/.match(l)
+        num_found = /[0-4]/.match(n)
      end
 
 
      @turn_typed = turn_typed
-     puts "turn_typed: " + turn_typed
+     puts "turn_typed" + turn_typed
       if @playerTP == @player1
         @playerTP = @player2
      else
         @playerTP = @player1
      end
 
-     puts "dans game:"
-     puts "here player TP: #{@playerTP}"
-     puts "here player 1 : #{@player1}"
-     puts "here player 2 : #{@player2}"
-     puts ""
-
+     puts "here player 1 #{@player1}"
+     puts "here player 2 #{@player2}"
+     puts "here player TP #{@playerTP}"
 
 
      return @turn_typed
@@ -88,12 +78,14 @@ class BoardCase
 #  affichage du tour joué
 
   def case_played(board_case, board_array, turn_typed)
-    puts "dasn boardCase:"
-    puts "player TP : #{@playerTP}"
-    puts "player 1  : #{@player1}"
-    puts "player 2  : #{@player2}"
 
-    if @playerTP == @player1
+    puts "player TP  #{@playerTP}"
+    puts "player 1   #{@player1}"
+    puts "player 2   #{@player2}"
+
+    @playerTP = "kiki"
+    if @playerTP == "kiki"
+      puts "e passe"
       if @turn_typed == "A1"
          @board_array == [["|x|_|_|"],["|_|_|_|"],["|_|_|_|"]]
        else
@@ -194,8 +186,8 @@ class Players
   attr_accessor :playerTP, :player1, :state1, :player2, :state2
 
   def initialize(playerTP, player1, state1, player2, state2)
-    state1 = "TP"
-    state2 = "TP"
+     state1 = "TP"
+     state2 = "TP"
      @player1 = player1
      @player2 = player2
      @state1  = state1
@@ -204,38 +196,39 @@ class Players
    end
 
 
-def players_entry(playerTP,player1, state1, player2, state2)
+def players_entry(player1, state1, player2, state2)
    while player1 == nil
-     print  "Joueur 1, saisissez votre pseudo:"
+     puts "Joueur 1, saisissez votre pseudo:"
      player1 = gets.chomp
      @player1 = player1
    end
    while player2 == nil
-     print "Joueur 2, saisissez votre pseudo:"
+     puts "Joueur 2, saisissez votre pseudo:"
      player2 = gets.chomp
      @player2 = player2
-     puts " "
    end
    puts "Status TP = To play, PL = Played, Wo = Won"
    puts "Hey!! Here will begin the game with #{@player1} (status #{@state1}) and #{@player2} (status #{@state2})  !! ) "
 
 #   return @player1
-   return @playerTP
+   return @player2
  end
+
+# def initialize
+
 
 
 end
 
-
+the_game  = Game.new
 the_board = Board.new(@board_array,@line_dep)
-the_player= Players.new(@playerTP, @player1, @state1, @player2, @state2)
-puts the_player.players_entry(@playerTP, @player1, @state1, @player2, @state2)
-the_game  = Game.new(@player1, @player2, @playerTP)
-
-the_game.turn_verification(@turn_typed)
+the_player= Players.new(@playeTP, @player1, @state1, @player2, @state2)
+puts the_player.players_entry(@player1, @state1, @player2, @state2)
+the_party = Players.new("", "",  "", "", "")
 the_shoot = BoardCase.new
-the_board.board_alim
+the_game.turn_verification(@turn_typed)
 the_shoot.case_played(@board_case, @board_array, @turn_typed)
+the_board.board_alim
 
 #turn_verification(@turn_typed)
 
